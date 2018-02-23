9b62c42ca756c602c2b7f50f1af1038e12cb94ee
Ali Baloch <ali.baloch6@gmail.com>
2018-02-23 18:27:53 +0400

    Paractice-Machine Learning

---
+++ Classification Template.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 07 12:06:58 2018
+
+@author: Ali Baloch
+"""
+# Classification Template
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+# Info are the user id the gender, age, estimated salary and this social network has several business clients which
+# can put their ads on social network and one of their client is a car company who has justlaunched their brand new
+# luxury SUV for a ridiculous price and we're trying to see which of the users of social network are going to buy
+# this brand new SUV and so the last column(Purchased) here tells if yes or no the user bought this SUV we are going to build
+# a model that is going to predict if a user is going to buy or not the SUV based on two variable which are going to
+# be age and estimated salary. and these will be the matrix of column
+X = dataset.iloc[:, [2, 3]].values 
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+
+#Fitting the classifier to the training set
+#Create your classifier here
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Make the confusion Matrix to evaluate the prediction
+from sklearn.metrics import confusion_matrix # function is always start with small letter
+cm = confusion_matrix(y_test, y_pred)
+ # array([[65,  3],
+ #      [ 8, 24]],  8 and 3 are incorrect prediction means we have 11 incorrect predictions and 65 and 42 are 
+ #correct it means 65+24 =89 correct prediciton
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+ 
+
+
+
+
+
+
+
+
+
+
